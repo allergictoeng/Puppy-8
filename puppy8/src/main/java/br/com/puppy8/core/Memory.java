6450daf7044cf5bd8d6626a3829603d2658c6308
@Allergictoeng <amaralrodrigues3@gmail.com>
2020-05-28 20:11:25 -0300

    First version of memory class and other git configs.

---
+++ puppy8/src/main/java/br/com/puppy8/core/Memory.java
@@ -0,0 +1,23 @@
+package br.com.puppy8.core;
+
+public class Memory {
+	static final int SIZE_4096 = 0x1000;
+	int[] memory;
+	
+	public Memory(int size) {
+		this.memory = new int[size];
+	}
+
+	public int size() {
+		return this.memory.length;
+	}
+
+	public int read(int position) {
+		return memory[position];
+	}
+
+	public void write(int data, int position) {
+		memory[position] = data;
+	}
+
+}
---
+++ puppy8/src/test/java/br/com/puppy8/core/MemoryTest.java
@@ -0,0 +1,38 @@
+package br.com.puppy8.core;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MemoryTest {
+	
+	private Memory memory;
+	
+	@Before
+	public void create() {
+		memory = new Memory(Memory.SIZE_4096);
+	}
+	
+	@Test
+	public void checkMemorySize() {
+		assertTrue((memory.size() == 4096));
+	}
+	
+	@Test
+	public void readInMemory() {
+		assertTrue(memory.read(0x0000) == 0);
+	}
+	
+	@Test
+	public void writeInMemory() {
+		memory.write(0x000A,0x0000);
+		
+		assertTrue((memory.read(0x0000) == 0x000A));
+		assertFalse((memory.read(0x0001) == 0x000A));
+		
+	}
+	
+	
+}
